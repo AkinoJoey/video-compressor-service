56cb1b725da885d2aa2b3b262e2227e339d871d6
AkinoJoey <gakiya213@gmail.com>
2023-04-26 22:15:52 +0900

    ダウンロードまでできるようにした

--- client.py
+++ client.py
@@ -1,15 +1,14 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
+from tkinter import messagebox
 import socket
 import os
 import json
 
-
 class Client:
     def __init__(self):
         self.BUFFER_SIZE = 4096
-
         self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.server_address = "127.0.0.1"
         self.server_port = 9999
@@ -18,6 +17,13 @@ class Client:
         self.file_extension = None
         self.main_menu = None
         self.option_menu = None
+        
+    def connect(self):
+        self.sock.connect((self.server_address, self.server_port))
+
+        menu_info_json = self.create_menu_info_json()
+        self.send_menu_info(menu_info_json)
+        self.send_video()
     
     def create_menu_info_json(self):
         menu_info = {
@@ -27,28 +33,62 @@ class Client:
             "option_menu":self.option_menu
         }
 
+        print(menu_info)
         return json.dumps(menu_info)
     
     def send_menu_info(self,json_file):
         print("sending menu info ...")
-        DATA_SIZE_SENDING_PER_ONE = 4
+        DATA_SIZE = 4
         json_file_bytes = json_file.encode("utf-8")
-        self.sock.sendall(len(json_file_bytes).to_bytes(DATA_SIZE_SENDING_PER_ONE),"big")
+        self.sock.sendall(len(json_file_bytes).to_bytes(DATA_SIZE,"big"))
+        self.sock.sendall(json_file_bytes)
+    
+    def send_video(self):
+        print("Sending video...")
         
-    def connect(self):
-        self.sock.connect((self.server_address, self.server_port))
-
-        menu_info_json = self.create_menu_info_json()
-        self.send_menu_info(menu_info_json)
-
-        # print("Sending video..")
-        
-        # with open(self.file_path, "rb") as video:
-        #     buffer = video.read()
-        #     self.sock.sendall(buffer)
+        with open(self.file_path, "rb") as video:
+            data = video.read()
+            STREAM_RATE = 4096
+            self.sock.sendall(len(data).to_bytes(STREAM_RATE,"big"))
+            self.sock.sendall(data)
             
-        # print("Done sending..")
+        print("Done sending...")
+        
+        self.wait_to_convert()
+        
+    def wait_to_convert(self):
+        STREAM_RATE = 4096
+        
+        while True:
+            message = self.sock.recv(STREAM_RATE).decode("utf-8")
+            if message == "done":
+                break
+    
+    def tell_server_want_to_download(self):
+        message = "download"
+        self.sock.sendall(message.encode("utf-8"))
+        
+        self.download_video()
+        
+    def download_video(self):
+        STREAM_RATE = 4096
+        data_length = int.from_bytes(self.sock.recv(STREAM_RATE),"big")
+        download_dir_path = os.path.join(os.getenv('USERPROFILE'), 'Downloads') if os.name  == "nt" else os.path.expanduser('~/Downloads')
+        download_video_full_path = os.path.join(download_dir_path,self.file_name) + self.file_extension
+        
+        try:
+            with open(download_video_full_path, "xb") as video:
+                print("downloading video...")
+                while data_length > 0:
+                    data = self.sock.recv(STREAM_RATE if STREAM_RATE >= data_length else data_length)
+                    video.write(data)
+                    data_length -= len(data)
+                    print(data_length)
 
+            print("Done downloading ...")
+            
+        except FileExistsError:
+            pass
 
 class ViewController:
     def __init__(self,client):
@@ -74,13 +114,13 @@ class ViewController:
         mainframe.rowconfigure(1, weight=1)
         
         # 上半分のフレーム
-        upper_half_frame = ttk.Frame(mainframe,borderwidth=2, relief='solid')
+        upper_half_frame = ttk.Frame(mainframe)
         upper_half_frame.grid(column=0, row=0,sticky=(N, W, E, S))
         upper_half_frame.columnconfigure(0, weight=1)
         upper_half_frame.rowconfigure(0, weight=1)
         
         # 下半分のフレーム
-        lower_half_frame = ttk.Frame(mainframe,borderwidth=2, relief='solid')
+        lower_half_frame = ttk.Frame(mainframe)
         lower_half_frame.grid(column=0,row=1,sticky=(N, W, E, S))
         lower_half_frame.columnconfigure(0, weight=1)
         lower_half_frame.columnconfigure(1, weight=1)
@@ -106,7 +146,7 @@ class ViewController:
         compress_frame.grid(column=0, row=0)
         compress_frame.columnconfigure(0, weight=1)
         compress_frame.rowconfigure(0, weight=1)
-        ttk.Button(compress_frame,text="圧縮",command=lambda: [self.create_compress_option_display(),self.set_main_menu("圧縮")]).grid(column=0, row=0)
+        ttk.Button(compress_frame,text="圧縮",command=lambda: [self.confirm_selected_video(),self.set_main_menu("圧縮")]).grid(column=0, row=0)
         
         # # 解像度ボタンの部分
         resolution_frame = ttk.Frame(lower_half_frame)
@@ -157,15 +197,26 @@ class ViewController:
     
     def display_file_name(self, file_name):
         self.file_name.set(file_name)
-    
-    def create_compress_option_display(self):
-        # option_windowの作成
+        
+    def create_new_window(self,title):
         option_window = Toplevel(self.root)
-        option_window.title("圧縮する")
+        option_window.title(title)
         option_window.geometry("420x220")
         option_window.resizable(False,False)
         option_window.columnconfigure(0, weight=1)
         option_window.rowconfigure(0, weight=1)
+        
+        return option_window
+    
+    def confirm_selected_video(self):
+        if self.file_name.get() == "":
+            messagebox.showerror(message="ファイルを選択してください")
+        else:
+            self.create_compress_option_window()
+            
+    def create_compress_option_window(self):
+        # option_windowの作成
+        option_window = self.create_new_window("圧縮レベル")
 
         # mainframeの作成
         mainframe = ttk.Frame(option_window)
@@ -175,22 +226,18 @@ class ViewController:
         mainframe.rowconfigure(1, weight=1)
         mainframe.rowconfigure(2, weight=1)
         mainframe.rowconfigure(3, weight=1)
-        mainframe.rowconfigure(4, weight=1)
-
-        # 圧縮レベルのラベル部分
-        ttk.Label(mainframe,text="圧縮レベル").grid(column=0,row=0)
 
         # radio button
-        compress_level = StringVar()
+        compress_level = StringVar(value="middle")
         high = ttk.Radiobutton(mainframe, text="high", variable=compress_level, value="high")
-        high.grid(column=0,row=1)
+        high.grid(column=0,row=0)
         middle = ttk.Radiobutton(mainframe, text="middle", variable=compress_level, value="middle")
-        middle.grid(column=0, row=2)
+        middle.grid(column=0, row=1)
         low = ttk.Radiobutton(mainframe, text="low", variable=compress_level, value="low")
-        low.grid(column=0, row=3)
+        low.grid(column=0, row=2)
 
         # start button
-        ttk.Button(mainframe, text="start",command=lambda:[self.set_option_menu(compress_level.get()),self.start_to_convert()]).grid(column=0, row=4)
+        ttk.Button(mainframe, text="start",command=lambda:[self.set_option_menu(compress_level.get()),self.start_to_convert(option_window)]).grid(column=0, row=3)
 
         # main manuの操作ができないように設定して、フォーカスを新しいウィンドウに移す
         option_window.grab_set()
@@ -202,8 +249,34 @@ class ViewController:
     def set_option_menu(self, option_menu):
         self.client.option_menu = option_menu
 
-    def start_to_convert(self):
-        self.client.create_menu_info_json()
+    def start_to_convert(self,option_window):
+        option_window.destroy()
+        prosessing_window = self.create_new_window("処理中")
+        
+        mainframe = ttk.Frame(prosessing_window,padding=50)
+        mainframe.grid(column=0, row=0,sticky=(N, W, E, S))
+        mainframe.columnconfigure(0, weight=1)
+        mainframe.rowconfigure(0, weight=1)
+        
+        progressbar = ttk.Progressbar(mainframe,orient=HORIZONTAL,mode='indeterminate')
+        progressbar.grid(column=0,row=0,sticky=(N, W, E, S))
+        progressbar.start(10)
+        
+        self.client.connect()
+        # self.create_download_window(prosessing_window)
+        
+    def create_download_window(self,prosessing_window):
+        prosessing_window.destroy()
+        download_window = self.create_new_window("処理完了")
+        
+        mainframe = ttk.Frame(download_window)
+        mainframe.grid(column=0, row=0,sticky=(N, W, E, S))
+        mainframe.columnconfigure(0, weight=1)
+        mainframe.rowconfigure(0, weight=1)
+        
+        download_btn = ttk.Button(mainframe, text="Download",command=lambda: self.client.tell_server_want_to_download()).grid(column=0, row=0)
+        
+        
 
 class Main():
     client = Client()
