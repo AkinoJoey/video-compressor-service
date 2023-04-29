801aec17ebbfc22c45f7554bf476acd3985b0b8d
AkinoJoey <gakiya213@gmail.com>
2023-04-29 18:19:01 +0900

    output名の関数作成。convert-videoからの分岐

--- server.py
+++ server.py
@@ -33,10 +33,10 @@ class Server:
         STREAM_RATE = 4096
         data_length = int.from_bytes(connect.recv(STREAM_RATE),"big")
         print(data_length)
-        file_name = "server-side-" + menu_info["file_name"] + menu_info["file_extension"]
+        original_file_name = menu_info["file_name"] + menu_info["file_extension"]
         
         try:
-            with open(file_name, "xb") as video:
+            with open(original_file_name, "xb") as video:
                 while data_length > 0:
                     data = connect.recv(STREAM_RATE if STREAM_RATE > data_length else data_length)
                     video.write(data)
@@ -48,18 +48,44 @@ class Server:
         except FileExistsError:
             pass
             
-        self.convert_video(file_name,connect)
+        self.convert_video(connect,menu_info,original_file_name)
         
-    def convert_video(self,file_name,connect):
-        level = 40
-        output_file_name = str(level) + "-" + file_name
-        print("start to convert the video")
-        compress_command = f"ffmpeg -n -i {file_name} -c:v libx264 -crf {level} -preset medium -tune zerolatency -c:a copy {output_file_name}"
+    def convert_video(self,connect,menu_info,original_file_name):
+        output_file_name = self.create_output_file_name(menu_info)
+        main_menu = menu_info["main_menu"]
         
-        subprocess.run(compress_command,shell=True)
+        if main_menu == "compress":
+            self.compress_video(original_file_name,menu_info,output_file_name)
+        
         print("end converting")
         
         self.report_to_end_converting(connect,output_file_name)
+    
+    def create_output_file_name(self,menu_info):
+        original_file_name = menu_info["file_name"]
+        main_menu = menu_info["main_menu"]
+        option_menu = menu_info["option_menu"]
+        file_extenstion = menu_info["file_extension"]
+        
+        return f"{original_file_name}-{main_menu}-{option_menu}{file_extenstion}"
+    
+    def compress_video(self,original_file_name,menu_info,output_file_name):
+        option_menu = menu_info["option_menu"]
+        high = 40
+        middle = 34
+        low = 28
+        
+        if option_menu == "high":
+            level = high
+        elif option_menu == "middle":
+            level = middle
+        else:
+            level = low
+            
+        print("start to convert the video")
+        compress_command = f"ffmpeg -n -i {original_file_name} -c:v libx264 -crf {level} -preset medium -tune zerolatency -c:a copy {output_file_name}"
+
+        subprocess.run(compress_command,shell=True)
     
     def report_to_end_converting(self,connect,file_name):
         message = "done"
@@ -95,7 +121,3 @@ class Main():
 if __name__ == "__main__":
     Main()
     
-    
-# ffmpeg -i usrs_original_video.mp4 -c:v libx264 -crf 40 -preset medium -tune zerolatency -c:a copy output-crf-40.mp4
-# ffmpeg -i test.mp4 -c:v libx264 -crf 40 -preset medium -tune zerolatency -c:a copy output-crf-40.mp4
-# ffmpeg -i test-1920x1080-30fps.mp4 -c:v libx264 -crf 28 -preset medium -tune zerolatency -c:a copy output-crf-28.mp4
