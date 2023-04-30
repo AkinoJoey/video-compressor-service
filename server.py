5017cafda68e2dc269e4c71a8e26872a7a1fe627
AkinoJoey <gakiya213@gmail.com>
2023-04-30 17:51:00 +0900

    動画保管場所の作成と削除の関数。でかい動画を受け取れるようにした。

--- server.py
+++ server.py
@@ -2,13 +2,20 @@ import socket
 import subprocess
 import json
 import sys
+import os
+import shutil
 
 class Server:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_address = "127.0.0.1"
         self.server_port = 9999
-        
+        self.temp_strage_dir_path = "./temp-strage-dir/"
+    
+    def check_and_mkdir_for_strage_dir_path(self):
+        if not os.path.exists(self.temp_strage_dir_path):
+            os.mkdir(self.temp_strage_dir_path)
+            
     def accpet(self):
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.server_address, self.server_port))
@@ -17,10 +24,18 @@ class Server:
         connect, address = self.sock.accept()
         print("Connected by", address)
         
-        while True:
-            menu_info =  self.receive_menu_info(connect)
-            self.receive_video(connect,menu_info)
+        try:
+            while True:
+                menu_info =  self.receive_menu_info(connect)
+                self.receive_video(connect,menu_info)
+        
+        except Exception as e:
+            print("Error" + str(e))
 
+        finally:
+            self.sock.close()
+            shutil.rmtree(self.temp_strage_dir_path)
+            
     def receive_menu_info(self,connect):
         DATA_SIZE = 4
         json_length = int.from_bytes(connect.recv(DATA_SIZE),"big")
@@ -33,12 +48,12 @@ class Server:
         STREAM_RATE = 4096
         data_length = int.from_bytes(connect.recv(STREAM_RATE),"big")
         print(data_length)
-        original_file_name = menu_info["file_name"] + menu_info["file_extension"]
+        original_file_name = self.temp_strage_dir_path + menu_info["file_name"] + menu_info["file_extension"]
         
         try:
-            with open(original_file_name, "xb") as video:
+            with open(original_file_name, "xb+") as video:
                 while data_length > 0:
-                    data = connect.recv(STREAM_RATE if STREAM_RATE > data_length else data_length)
+                    data = connect.recv(data_length if data_length <= STREAM_RATE else STREAM_RATE)
                     video.write(data)
                     data_length -= len(data)
                     print(data_length)
@@ -47,11 +62,12 @@ class Server:
             
         except FileExistsError:
             pass
+        
             
         self.convert_video(connect,menu_info,original_file_name)
         
     def convert_video(self,connect,menu_info,original_file_name):
-        output_file_name = self.create_output_file_name(menu_info)
+        output_file_name = self.temp_strage_dir_path + self.create_output_file_name(menu_info)
         main_menu = menu_info["main_menu"]
         
         if main_menu == "compress":
@@ -104,18 +120,27 @@ class Server:
         
     def send_converted_video(self,file_name,connect):
         print("Sending video...")
+        STREAM_RATE = 4096
         
         with open(file_name, "rb") as video:
-            data = video.read()
-            STREAM_RATE = 4096
-            connect.sendall(len(data).to_bytes(STREAM_RATE,"big"))
-            connect.sendall(data)
+            video.seek(0, os.SEEK_END)
+            data_size = video.tell()
+            video.seek(0,0)
+            connect.sendall(data_size.to_bytes(STREAM_RATE,"big"))
+            
+            data = video.read(4096)
+            
+            while data:
+                print("sending...")
+                connect.send(data)
+                data = video.read(4096)
             
         print("Done sending...")
         
             
 class Main():
     server = Server()
+    server.check_and_mkdir_for_strage_dir_path()
     server.accpet()
 
 if __name__ == "__main__":
